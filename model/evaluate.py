264342e2de7c7b0866ba049c97e4cc64b0c7e320
LazyIce <1542752218@qq.com>
2019-11-25 14:02:09 -0500

    add evaluate.py

---
+++ model/evaluate.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def sift_query(img_path):
+
+
+
+def cnn_query(img_path):
+    
+
+
+def rank_rerank(img_path, k):
+    
+
+
+def average_score(img_path, w):
+    
