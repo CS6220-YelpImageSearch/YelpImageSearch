0050a1911f482ceee8ca1af95f53fa0f7590d851
LazyIce <1542752218@qq.com>
2019-11-25 14:38:59 -0500

    update evaluate.py

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,51 +1,63 @@
-import matplotlib.pyplot as plt
-import src as sc
+import sys
+import os
 import h5py
-import os.path as osp
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow.keras.backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image as kimage
 
-def load_sift():
-    name_dataset = 'yelp'
-    vocab_filename = '4000vocab.h5'
-
-    if osp.exists(vocab_filename):
-        with h5py.File(vocab_filename, 'r') as hf:
-            vocab = hf[name_dataset][:]
-    else:
-        raise "vocab file does not exist"
-
-    feats_filename = 'feats.h5'
-    if osp.exists(feats_filename):
-        with h5py.File(feats_filename, 'r') as hf:
-            train_feats = hf[name_dataset][:]
-    else:
-        raise "feats file does not exist"
-        
-    paths_filename = 'paths.h5'
-    if osp.exists(paths_filename):
-        with h5py.File(paths_filename, 'r') as hf:
-            paths = hf[name_dataset][:]
-    else:
-        raise "paths file does not exist"
-
-    return (vocab, train_feats, paths)
-
-def predict_sift(test_path, vocab, train_feats, max_num=3):
-    test_feats = sc.get_bags_of_sifts([test_path], vocab)
-    predicted_indexes = sc.nearest_neighbor_classify(train_feats, test_feats)
-    
-    return paths[predicted_indexes[0, :max_num]]
 
 def sift_query(img_path):
-
+    return 0
 
 
 def cnn_query(img_path):
-    
+    h5f = h5py.File('./output/feature/LasVegasFoodFeatures', 'r')
+    features = h5f['dataset_1'][:]
+    img_names = h5f['dataset_2'][:]
+    h5f.close()
+
+    best_model = load_model('./output/model/best_model.hdf5', compile = False)
+    feature_extractor = K.function([best_model.layers[0].input], [best_model.layers[-4].output])
+    img = kimage.load_img(img_path, target_size=(299, 299))
+    img = kimage.img_to_array(img)                    
+    img = np.expand_dims(img, axis=0)         
+    img /= 255. 
+    query_feature = feature_extractor([img])[0][0]
+    scores = np.dot(query_feature, features.T)
+    rank_ID = np.argsort(scores)[::-1]
+    rank_score = scores[rank_ID]
+
+    max_res = 3
+    img_list = [img_names[index] for i, index in enumerate(rank_ID[0:max_res])]
+
+    for i, img in enumerate(img_list):
+        image = mpimg.imread('./data/LV_photos' + '/' + str(img, 'utf-8'))
+        plt.title('search output %d' %(i+1))
+        plt.imshow(image)
+        plt.show()
 
 
 def rank_rerank(img_path, k):
-    
+    return 0
 
 
 def average_score(img_path, w):
-    
+    return 0
+
+
+if __name__ == '__main__':
+    method = sys.argv[1]
+    img_path = sys.argv[2]
+    if method == 'sift':
+        sift_query(img_path)
+    elif method == 'cnn':
+        cnn_query(img_path)
+    elif method == 'rerank':
+        rank_rerank(img_path)
+    elif method == 'average':
+        average_score(img_path)
+    else:
+        print('We only provide 4 methods: sift, cnn, rerank and average!')
